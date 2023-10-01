1c2666f37ec802eac54fd9d05a9e9318bd7226dd
JTew27 <145987450+JTew27@users.noreply.github.com>
2023-10-01 20:18:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("in a long career for Yorkshire Geoffrey Boycott played 609 matches batted 1014 times was not out 162 times and scored 48426 runs ")
+
+avg = (48246)/(1014-162)
+
+print ("batting average:",avg)
+
